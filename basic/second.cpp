6e891259dc20c284919c1a387376ffb3e5dcdba8
atuldemond <atuldemond@gmail.com>
2024-09-15 21:16:06 +0530

    updating

---
+++ basic/second.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // VARIABLE AND DATATYPES
+    // declaring varibal --4bytes
+    int one;
+    int man = 21;
+    int a = 21, b = 21;
+    cout << a + b << endl;
+
+    // char varibale -- 1 bytes
+    char c = 'a';
+    cout << c << endl;
+
+    // float
+    float f = 2.2;
+    cout << f << endl;
+
+    // double
+    double d = 2323.343;
+    cout << d << endl;
+
+    // boolean
+
+    bool bl = false;
+    cout << bl << endl;
+};
