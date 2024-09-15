48b14da1221fa78d00fcc04b496875333de6280b
atuldemond <atuldemond@gmail.com>
2024-09-15 21:15:34 +0530

    cin and cout

---
+++ if_else/first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // input and output
+    int a, b;
+    cout << "enter your number first number ::\n";
+    cin >> a;
+    cout << "enter Your Second Number ::\n";
+    cin >> b;
+    cout << "sum of two number is ::" << a + b;
+};
