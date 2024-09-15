40ab8a479a536e4d0ed8115eea2ad98dc9abfc8e
atuldemond <atuldemond@gmail.com>
2024-09-15 18:37:29 +0530

    checking

--- basic/first.cpp
+++ basic/first.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-cout<<"Day one c++"<<endl;
-cout<<3+5;
-return 0;
+int main()
+{
+    cout << "Day one c++" << endl;
+    cout << 3 + 5 << endl;
+    cout << "code formating with c++";
+    return 0;
 };
