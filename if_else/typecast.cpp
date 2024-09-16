ebc78a654a83f894aef784ee5f60d3ad37958edf
atuldemond <atuldemond@gmail.com>
2024-09-16 21:12:25 +0530

    second cm by another pc

---
+++ if_else/typecast.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a =2332;
+    char d= 'x';
+    d=a;
+    cout<<d;
+
+    // we can store data from higher value to low but if we store
+    // low to high then , we loss of data
+};
