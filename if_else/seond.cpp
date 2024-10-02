016bff03a1ab4be81652ca8f1148a429c508908e
atuldemond <atuldemond@gmail.com>
2024-10-02 07:26:34 +0530

    1/90

---
+++ if_else/seond.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // adding to number using input
+    cout << "Enter Your First Number\n";
+    int a;
+    cin >> a;
+    cout << "Enter Your Second Numebr\n";
+    int b;
+    cin >> b;
+    cout << "Sum of Both Number is " << a + b;
+}
